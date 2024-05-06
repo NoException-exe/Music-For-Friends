f0df57776b50918d592fd85b37123547589e743c
NoException-exe <gemer942@gmail.com>
2024-05-05 22:36:08 -0300

    fix: interaction event

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -2,7 +2,7 @@ const { Events } = require("discord.js");
 
 module.exports = {
   name: Events.InteractionCreate,
-  once: true,
+  once: false,
   async execute(interaction) {
     if (!interaction.isChatInputCommand()) return;
 
